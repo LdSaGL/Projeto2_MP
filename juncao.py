148bc5e6c25cf60eeb0b4e0a1cd8008669c49b26
LdSaGL <lucas.sa.licio@gmail.com>
2022-07-31 15:02:36 -0300

    Teste para verificar qual arquivo sera aberto

---
+++ juncao.py
@@ -0,0 +1,3 @@
+#Informar os arquivos que participarao do processo de juncao
+ArquivoX = input("Insira o nome do primeiro arquivo: ")
+ArquivoY = input("Insira o nome do segundo arquivo: ")
---
+++ testa_juncao.py
@@ -0,0 +1,6 @@
+import juncao
+
+def testeArquivoAberto1():
+    assert juncao.ArquivoX == "Arquivo1.txt" 
+def testeArquivoAberto2():
+    assert juncao.ArquivoY == "Arquivo2.txt"
