5c2f8031893a1d2c7cf568ea701317bebd23d56d
LdSaGL <lucas.sa.licio@gmail.com>
2022-07-31 22:12:47 -0300

    Formatacao pylint

--- juncao.py
+++ juncao.py
@@ -1,4 +1,6 @@
 """ Funcao que extrai os numeros de um arquvio e retorna uma lista"""
+
+
 def extraiNumeros(z):
     arquivoAberto = open(z, 'r')
     numerosArquivo = []
@@ -7,20 +9,32 @@ def extraiNumeros(z):
     arquivoAberto.close()
     return numerosArquivo
 
-""" Funcao que junta as listas extraidas de cada um dos dois arquivos e retorna uma lista"""
+
+""" Funcao que junta as listas extraidas de cada um dos dois arquivos e
+retorna uma lista"""
+
+
 def juntaNumeros(arquivoX, arquivoY):
     lista_X = extraiNumeros(arquivoX)
     lista_Y = extraiNumeros(arquivoY)
     lista_X.extend(lista_Y)
     return lista_X
 
-""" Funcao que pega a lista obtida na funcao juntaNumeros, ordena e retorna uma lista"""
+
+""" Funcao que pega a lista obtida na funcao juntaNumeros, ordena e
+retorna uma lista"""
+
+
 def ordenaLista(arquivoX, arquivoY):
     lista_unida = juntaNumeros(arquivoX, arquivoY)
     lista_unida.sort()
     return lista_unida
 
-""" Funcao que cria o arquivo final e coloca os numeros da lista ordenada seguindo o padrao de formatacao fornecido no roteiro"""
+
+""" Funcao que cria o arquivo final e coloca os numeros da lista
+ordenada seguindo o padrao de formatacao fornecido no roteiro"""
+
+
 def executar(arquivoX, arquivoY, nomeArquivo):
     lista = ordenaLista(arquivoX, arquivoY)
     arquivo_final = open(nomeArquivo, "x")
@@ -28,14 +42,15 @@ def executar(arquivoX, arquivoY, nomeArquivo):
         arquivo_final.write(termo+'\n')
 
 
-a = "Arquivo1.txt"
-b = "Arquivo2.txt"
+# Variaveis de teste - desconsiderar ao rodar o codigo
+# a = "Arquivo1.txt"
+# b = "Arquivo2.txt"
 
-
-"""print('Informe quais os arquivos que deverao ser unidos seguindo o formato: "NomeDoArquivo.txt"')
+print('Siga o formato para qualquer input: "NomeDoArquivo.txt"')
+print('Informe quais os arquivos que deverao ser unidos')
 a = input('Primeiro arquivo: ')
-b = input('Segundo arquivo: ')
+b = input('Segundo arquivo : ')
 print()
-print('Informe como deseja chamar o arquivo final seguindo o formato: "NomeDoArquivo.txt"')
+print('Informe como deseja nomear o arquivo final')
 nome = input('Nome do arquivo: ')
-executar(a, b, nome)"""
+executar(a, b, nome)
